3257d5f049a39854902cfaac3b649ce4685f0fd0
hanslelee <hssarah13@gmail.com>
2022-07-20 17:41:37 +0900

    colors - 추후 필요한 색 있을때마다 추가

--- lib/src/view/utils/colors.dart
+++ lib/src/view/utils/colors.dart
@@ -7,3 +7,9 @@ const Color grey153 = Color.fromRGBO(153, 153, 153, 1.0);
 const Color grey183 = Color.fromRGBO(183, 183, 183, 1.0);
 const Color grey216 = Color.fromRGBO(216, 216, 216, 1.0);
 const Color grey239 = Color.fromRGBO(239, 239, 239, 1.0);
+
+const Color green = Color(0xFFA2C25E);
+const Color navy = Color(0xFF2F4858);
+const Color pastelGreen = Color(0x60A3E21B);
+const Color pastelYellow = Color(0x60FFCF23);
+const Color pastelBlue = Color(0x6060B6ED);
