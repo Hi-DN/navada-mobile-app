2b10a3eee520000e0e4168b2e24d7baaeb3c7761
hanslelee <hssarah13@gmail.com>
2022-07-20 17:39:58 +0900

    colors - 추후 필요한 색 있을때마다 추가

---
+++ lib/src/view/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color.fromRGBO(0, 0, 0, 1.0);
+const Color white = Color.fromRGBO(255, 255, 255, 1.0);
+
+const Color grey153 = Color.fromRGBO(153, 153, 153, 1.0);
+const Color grey183 = Color.fromRGBO(183, 183, 183, 1.0);
+const Color grey216 = Color.fromRGBO(216, 216, 216, 1.0);
+const Color grey239 = Color.fromRGBO(239, 239, 239, 1.0);
