63a1bd4e47621cd8cb9ccd1c17a6cb7aab0a3320
hanslelee <hssarah13@gmail.com>
2022-07-21 18:47:53 +0900

    http client 및 Product model

---
+++ lib/src/business_logic/api/http_client.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class HttpClient {
+  static final HttpClient _instance = HttpClient._internal();
+  final String baseUrl = 'http://192.168.25.12:8000/';
+  // final String baseUrl = 'http://10.0.2.2:8000/';
+  // final String baseUrl = 'http://localhost:8000/';
+
+  String accessToken = '';
+
+  factory HttpClient() {
+    return _instance;
+  }
+
+  HttpClient._internal();
+
+  Future<Map<String, dynamic>> getRequest(String url,
+      {bool tokenYn = false}) async {
+    http.Response response;
+    if (tokenYn) {
+      var headers = {'ACCESS_TOKEN': accessToken};
+      response = await http.get(Uri.parse(baseUrl + url), headers: headers);
+    } else {
+      response = await http.get(Uri.parse(baseUrl + url));
+    }
+    debugPrint('url : ${baseUrl + url}');
+    debugPrint('statusCode : ${response.statusCode}');
+    debugPrint('response body : ${utf8.decode(response.bodyBytes)}');
+
+    return jsonDecode(utf8.decode(response.bodyBytes));
+  }
+
+  Future<Map<String, dynamic>> postRequest(
+      String url, Map<String, dynamic> body,
+      {bool tokenYn = false}) async {
+    http.Response response;
+    Map<String, String> headers;
+    if (tokenYn) {
+      headers = {
+        'Content-Type': 'application/json',
+        'ACCESS_TOKEN': accessToken
+      };
+    } else {
+      headers = {'Content-Type': 'application/json'};
+    }
+    response = await http.post(Uri.parse(baseUrl + url),
+        body: jsonEncode(body), headers: headers);
+    debugPrint('url : ${baseUrl + url}');
+    debugPrint('statusCode : ${response.statusCode}');
+    debugPrint('response body : ${utf8.decode(response.bodyBytes)}');
+    return jsonDecode(utf8.decode(response.bodyBytes));
+  }
+
+  Future<Map<String, dynamic>> putRequest(String url, Map<String, dynamic> body,
+      {bool tokenYn = false}) async {
+    http.Response response;
+    Map<String, String> headers;
+    if (tokenYn) {
+      headers = {
+        'Content-Type': 'application/json',
+        'ACCESS_TOKEN': accessToken
+      };
+    } else {
+      headers = {'Content-Type': 'application/json'};
+    }
+    response = await http.put(Uri.parse(baseUrl + url),
+        body: jsonEncode(body), headers: headers);
+    debugPrint('url : ${baseUrl + url}');
+    debugPrint('statusCode : ${response.statusCode}');
+    debugPrint('response body : ${response.body}');
+    return jsonDecode(response.body);
+  }
+
+  Future<Map<String, dynamic>> deleteRequest(String url,
+      {bool tokenYn = false}) async {
+    http.Response response;
+    Map<String, String> headers;
+    if (tokenYn) {
+      headers = {
+        'Content-Type': 'application/json',
+        'ACCESS_TOKEN': accessToken
+      };
+    } else {
+      headers = {'Content-Type': 'application/json'};
+    }
+    response = await http.delete(Uri.parse(baseUrl + url), headers: headers);
+
+    debugPrint('url : ${baseUrl + url}');
+    debugPrint('statusCode : ${response.statusCode}');
+    debugPrint('response body : ${response.body}');
+
+    return jsonDecode(response.body);
+  }
+
+  Future<Map<String, dynamic>> patchRequest(String url,
+      {bool tokenYn = false}) async {
+    http.Response response;
+    Map<String, String> headers;
+    if (tokenYn) {
+      headers = {
+        'Content-Type': 'application/json',
+        'ACCESS_TOKEN': accessToken
+      };
+    } else {
+      headers = {'Content-Type': 'application/json'};
+    }
+    response = await http.patch(Uri.parse(baseUrl + url), headers: headers);
+
+    debugPrint('url : ${baseUrl + url}');
+    debugPrint('statusCode : ${response.statusCode}');
+    debugPrint('response body : ${response.body}');
+
+    return jsonDecode(response.body);
+  }
+}
--- lib/src/view/ui/login.dart
+++ lib/src/view/ui/login.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:navada_mobile_app/src/view/utils/custom_appbar.dart';
+import 'package:navada_mobile_app/src/view/utils/screen_size.dart';
 
 class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    ScreenSize size = ScreenSize();
+    size.setMediaSize(MediaQuery.of(context).size);
+
+    // api test
+    // getProduct(1);
+
     return Scaffold(
+        appBar: CustomAppBar(titleText: "Appbar Test"),
         body: Container(
-      child: const Text("하염"),
-    ));
+          child: const Text("하염"),
+        ));
   }
 }
