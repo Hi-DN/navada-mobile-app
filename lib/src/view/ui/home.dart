5ea3460a58d993115f0b2633629fd7dbbc7ae6b7
minji1110 <kalswl991110@gmail.com>
2022-07-25 16:07:34 +0900

    floating button 추가

--- lib/src/view/ui/home.dart
+++ lib/src/view/ui/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:navada_mobile_app/src/business_logic/user/user_model.dart';
-import 'package:navada_mobile_app/src/business_logic/user/user_provider.dart';
 import 'package:navada_mobile_app/src/view/ui/heart_list.dart';
 import 'package:navada_mobile_app/src/view/ui/my_page.dart';
 import 'package:navada_mobile_app/src/view/utils/colors.dart';
-import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   static const routeName = 'home';
@@ -30,31 +28,52 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    user = Provider.of<UserProvider>(context, listen: false).userModel;
+    _currIndex = 0;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('+ 버튼 클릭');
+        },
+        tooltip: 'Increment',
+        backgroundColor: green,
+        elevation: 0.0,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SafeArea(
         child: _widgetList.elementAt(_currIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: green,
-        currentIndex: _currIndex,
-        onTap: _onMenuTapped,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'menu'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_border_outlined), label: 'favorite'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline), label: 'person')
-        ],
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildMenu(0, Icons.home_outlined),
+            _buildMenu(1, Icons.menu),
+            const SizedBox(width: 20.0),
+            _buildMenu(2, Icons.favorite_border_outlined),
+            _buildMenu(3, Icons.person_outline),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenu(int index, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: IconButton(
+        onPressed: () {
+          _onMenuTapped(index);
+        },
+        icon: Icon(
+          icon,
+          size: 30.0,
+          color: _currIndex == index ? green : grey153,
+        ),
       ),
     );
   }
