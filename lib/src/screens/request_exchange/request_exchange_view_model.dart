6f8fa9949af0f8dd14b439e5f166ae25cb4082b9
minji1110 <kalswl991110@gmail.com>
2022-09-15 22:05:04 +0900

    다중선택 구현

--- lib/src/screens/request_exchange/request_exchange_view_model.dart
+++ lib/src/screens/request_exchange/request_exchange_view_model.dart
@@ -1,3 +1,27 @@
 import 'package:flutter/material.dart';
 
-class RequestExchangeViewModel extends ChangeNotifier {}
+class RequestExchangeViewModel extends ChangeNotifier {
+  List<int> _requestProductIdList = [];
+  List<int> get requestProductIdList => _requestProductIdList;
+
+  bool _isListEmpty = true;
+  bool get isListEmpty => _isListEmpty;
+
+  void addProductId(int productId) {
+    _requestProductIdList.add(productId);
+    _isListEmpty = false;
+
+    notifyListeners();
+  }
+
+  void removeProductId(int productId) {
+    _requestProductIdList.remove(productId);
+    if (_requestProductIdList.isEmpty) _isListEmpty = true;
+
+    notifyListeners();
+  }
+
+  bool isSelectedId(int productId) {
+    return _requestProductIdList.contains(productId);
+  }
+}
