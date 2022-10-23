c15d38729a715850700dcf1af2d602769b19701d
hanslelee <hssarah13@gmail.com>
2022-10-23 22:50:04 +0900

    물품 수정 화면

---
+++ lib/src/screens/modify_product/modify_product_view_model.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:navada_mobile_app/src/utilities/enums.dart';
+
+class ModifyProductViewModel extends ChangeNotifier {
+  ModifyProductViewModel(this._productName, this._productCategory, this._productPrice, this._productExchangeCost, this._productExplanation);
+  
+  String? _productName;
+  Category? _productCategory;  
+  int? _productPrice;
+  int? _productExchangeCost;
+  String? _productExplanation;
+
+  String? get productName => _productName;
+  Category? get productCategory => _productCategory;
+  int? get productPrice => _productPrice;
+  int? get productExchangeCost => _productExchangeCost;
+  String? get productExplanation => _productExplanation;
+
+  setProductName(String productName) {_productName = productNameController.value.text;}
+  setProductCategory(Category productCategory) {_productCategory = productCategory; notifyListeners();}
+  setProductPrice(int productPrice) {_productPrice = int.parse(productPriceController.value.text);}
+  setProductExchangeCost(int productExchangeCost) {_productExchangeCost = int.parse(productExchangeCostController.value.text);}
+  setProductExplanation(String productExplanation) {_productExplanation = productExplanationController.value.text;}
+
+  late TextEditingController productNameController = TextEditingController(text: _productName);
+  late TextEditingController productPriceController = TextEditingController(text: _productPrice.toString());
+  late TextEditingController productExchangeCostController = TextEditingController(text: _productExchangeCost.toString());
+  late TextEditingController productExplanationController = TextEditingController(text: _productExplanation);
+
+  final FocusNode productNameFNode = FocusNode();
+  final FocusNode productCategoryFNode = FocusNode();
+  final FocusNode productPriceFNode = FocusNode();
+  final FocusNode productExchangeCostFNode = FocusNode();
+  final FocusNode productExplanationFNode = FocusNode();
+
+  bool checkValidProductName() {
+    if(_productName == null || _productName == "" || productNameController.value.text.isEmpty) return false;
+    return true;
+  }
+
+  bool checkValidProductCategory() {
+    if(_productCategory == null) return false;
+    return true;
+  }
+
+  bool checkValidProductPrice() {
+    if(_productPrice == null || _productPrice! < 0 || productPriceController.value.text.isEmpty) return false;
+    return true;
+  }
+
+  bool checkValidProductExchangeCost() {
+    debugPrint(_productPrice.toString());
+    if(_productExchangeCost == null || productExchangeCostController.value.text.isEmpty || _productExchangeCost! < 0 || _productExchangeCost! > _productPrice!) return false;
+    return true;
+  }
+
+  bool checkValidProductExplanation() {
+    if(_productExplanation == null || _productExplanation == "" || productExplanationController.value.text.isEmpty) return false;
+    return true;
+  }
+}
