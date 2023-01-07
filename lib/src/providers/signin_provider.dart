7c913204603c3df6a5d5c6507ddb83bb169e2096
hanslelee <hssarah13@gmail.com>
2023-01-07 22:39:46 +0900

    로그인 리팩토링

---
+++ lib/src/providers/signin_provider.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:navada_mobile_app/src/models/oauth/signIn_model.dart';
+import 'package:navada_mobile_app/src/models/user/user_service.dart';
+import 'package:navada_mobile_app/src/utilities/enums.dart';
+
+
+class SignInProvider with ChangeNotifier {
+  UserService userService = UserService();
+
+  Future<OAuthToken?> getKakaoToken() async {
+    bool isInstalled = await isKakaoTalkInstalled();
+    OAuthToken? token;
+    if (isInstalled) {
+      try {
+          token = await UserApi.instance.loginWithKakaoTalk();
+      } catch (error) {
+        debugPrint('카카오톡으로 로그인 실패 $error');
+        if (error is PlatformException && error.code == 'CANCELED') {
+            return null;
+        }
+        try {
+            token = await UserApi.instance.loginWithKakaoAccount();
+        } catch (error) {
+            debugPrint('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        token = await UserApi.instance.loginWithKakaoAccount();
+      } catch (error) {
+        debugPrint('카카오계정으로 로그인 실패 $error');
+      }
+    }
+    return token;
+  }
+
+  Future<KakaoUserInfo> getKakaoUserInfo(OAuthToken? token) async {
+    final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+
+    final response = await http.get(
+      url,
+      headers: {
+        HttpHeaders.authorizationHeader: 'Bearer ${token!.accessToken}'
+      },
+    );
+
+    final profileInfo = json.decode(response.body);
+    String email = profileInfo["kakao_account"]["email"].toString();
+    String nickname = profileInfo["properties"]["nickname"].toString();
+    return KakaoUserInfo(email, nickname);
+  }
+
+  Future<SignInResponse> signInByOAuth(String email, String nickname, SignInPlatform platform) async {
+    return await userService.signInByOAuth(email, nickname, platform);
+  }
+}
+
+class KakaoUserInfo {
+  late String email;
+  late String nickname;
+  KakaoUserInfo(this.email, this.nickname);
+}
--- lib/src/screens/signin/signin.dart
+++ lib/src/screens/signin/signin.dart
@@ -1,164 +1,161 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-import 'package:flutter/services.dart';
 import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:navada_mobile_app/src/models/oauth/signIn_model.dart';
 import 'package:navada_mobile_app/src/models/user/user_provider.dart';
-import 'package:navada_mobile_app/src/models/user/user_service.dart';
+import 'package:navada_mobile_app/src/providers/signin_provider.dart';
 import 'package:navada_mobile_app/src/screens/signin/signup.dart';
 import 'package:navada_mobile_app/src/utilities/enums.dart';
-import 'package:navada_mobile_app/src/widgets/custom_appbar.dart';
+import 'package:navada_mobile_app/src/widgets/colors.dart';
 import 'package:navada_mobile_app/src/widgets/custom_navigation_bar.dart';
 import 'package:navada_mobile_app/src/widgets/screen_size.dart';
 import 'package:navada_mobile_app/src/widgets/space.dart';
+import 'package:navada_mobile_app/src/widgets/text_style.dart';
 import 'package:provider/provider.dart';
 
 
 class SignIn extends StatelessWidget {
-  const SignIn({Key? key}) : super(key: key);
+  SignIn({Key? key}) : super(key: key);
+
+  late BuildContext? _context;
 
   @override
   Widget build(BuildContext context) {
     ScreenSize size = ScreenSize();
     size.setMediaSize(MediaQuery.of(context).size);
+    _context = context;
 
     return Scaffold(
-        appBar: CustomAppBar(titleText: "Appbar Test"),
-        body: Center(
-          child: Column(
+      backgroundColor: white,
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Space(height: 40),
+          _logo(),
+          const Space(height: 170),
+          _loginTestBtn(),
+          const Space(height: 30),
+          Row(mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                child: const Text(
-                  '로그인하기',
-                ),
-                onPressed: () {
-                  UserProvider userProvider = Provider.of(context, listen: false);
-                  userProvider.notifyListeners();
-                  Navigator.of(context).pushNamed(CustomNavigationBar.routeName);
-                },
-              ),
-              const Space(height: 20,),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 255, 204, 0)),
-                child: const Text(
-                  '카톡 로그인하기',
-                ),
-                onPressed: () async {
-                  bool isInstalled = await isKakaoTalkInstalled();
-                  OAuthToken? token;
-                  UserService userService = UserService();
+            _kakaoBtn(),
+            const Space(width: 20),
+            _googleBtn(),
+            const Space(width: 20),
+            _naverBtn()
+          ])
+        ],
+      ));
+  }
 
-                    if (isInstalled) {
-                      try {
-                          token = await UserApi.instance.loginWithKakaoTalk();
-                      } catch (error) {
-                        debugPrint('카카오톡으로 로그인 실패 $error');
-                        if (error is PlatformException && error.code == 'CANCELED') {
-                            return;
-                        }
-                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                        try {
-                            token = await UserApi.instance.loginWithKakaoAccount();
-                        } catch (error) {
-                            debugPrint('카카오계정으로 로그인 실패 $error');
-                        }
-                      }
-                    } else {
-                      
-                      try {
-                        token = await UserApi.instance.loginWithKakaoAccount();
-                      } catch (error) {
-                        debugPrint('카카오계정으로 로그인 실패 $error');
-                      }
-                  }
-                  final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+  Widget _logo() {
+    ScreenSize size = ScreenSize();
+    return Image.asset(
+      'assets/images/logo.png',
+      width: size.getSize(280.0),
+      height: size.getSize(160.0),
+    );
+  }
 
-                  final response = await http.get(
-                    url,
-                    headers: {
-                      HttpHeaders.authorizationHeader: 'Bearer ${token!.accessToken}'
-                    },
-                  );
+  Widget _loginTestBtn() {
+    return GestureDetector(
+      onTap: () {
+        UserProvider userProvider = Provider.of(_context!, listen: false);
+        userProvider.notifyListeners();
+        Navigator.of(_context!).pushNamed(CustomNavigationBar.routeName);
+      },
+      child: const R14Text(text: '로그인(테스트용)', textColor: grey153,)
+    );
+  }
 
-                  final profileInfo = json.decode(response.body);
-                  print(profileInfo.toString());
-                  print(profileInfo["kakao_account"]["email"]);
-                  print(profileInfo["properties"]["nickname"]);
-                  String email = profileInfo["kakao_account"]["email"].toString();
-                  String nickname = profileInfo["properties"]["nickname"].toString();
-
-                  SignInResponse? signInResponse = await userService.signInByOAuth(email, nickname, SignInPlatform.KAKAO);
-
-                    Provider.of<UserProvider>(context, listen: false).setOAuthInfo(email, SignInPlatform.KAKAO);
-                    if (signInResponse.user == null) {
-                      // 회원가입 진행
-                      // Provider.of<UserProvider>(context, listen: false).setOAuthInfo(signInResponse.oauth);
-                      Navigator.push(context,MaterialPageRoute(builder: (BuildContext context) => const SignUp()));
-
-                    } else {
-                      // 로그인 진행
-                      Provider.of<UserProvider>(context, listen: false).setUser(signInResponse.user!, email, SignInPlatform.KAKAO);
-                      Navigator.of(context).pushNamed(CustomNavigationBar.routeName);
-                    }
-                },
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 255, 255, 255)),
-                child: const Text(
-                  '구글 로그인하기',
-                ),
-                onPressed: () async {
-                  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-                  UserService userService = UserService();
-                  
-                  if(googleUser != null){
-                    SignInResponse? response = await userService.signInByOAuth(googleUser.email, googleUser.displayName ?? "", SignInPlatform.GOOGLE);
-                    if (response.user == null) {
-                      // 회원가입 진행
-                      Provider.of<UserProvider>(context, listen: false).setOAuthInfo(googleUser.email, SignInPlatform.GOOGLE);
-                      Navigator.push(context,MaterialPageRoute(builder: (BuildContext context) => const SignUp()));
-
-                    } else {
-                      // 로그인 진행
-                      Provider.of<UserProvider>(context, listen: false).setUser(response.user!, googleUser.email, SignInPlatform.GOOGLE);
-                      Navigator.of(context).pushNamed(CustomNavigationBar.routeName);
-                    }
-                  }
-
-                  
-                },
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 96, 206, 57)),
-                child: const Text(
-                  '네이버 로그인하기',
-                ),
-                onPressed: () async {
-                  final NaverLoginResult result = await FlutterNaverLogin.logIn();
-                  UserService userService = UserService();
-                  
-                  SignInResponse? response = await userService.signInByOAuth(result.account.email, result.account.nickname, SignInPlatform.NAVER);
-                  if (response.user == null) {
-                    // 회원가입 진행
-                    Provider.of<UserProvider>(context, listen: false).setOAuthInfo(result.account.email, SignInPlatform.NAVER);
-                    Navigator.push(context,MaterialPageRoute(builder: (BuildContext context) => const SignUp()));
-
-                  } else {
-                    // 로그인 진행
-                    Provider.of<UserProvider>(context, listen: false).setUser(response.user!, result.account.email, SignInPlatform.NAVER);
-                    Navigator.of(context).pushNamed(CustomNavigationBar.routeName);
-                  }
-                  debugPrint(result.toString());
-                },
-              ),
-            ],
+  Widget _kakaoBtn() {
+    return Card(
+      elevation: 18.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFFEE500),
+      margin: EdgeInsets.zero,
+      child: Ink.image(
+        image: const AssetImage('assets/images/kakao_logo.png'),
+        width: 60,
+        height: 60,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(35.0),
           ),
-        ));
+          onTap: () async {
+            SignInProvider provider = Provider.of<SignInProvider>(_context!, listen: false);
+            OAuthToken? token = await provider.getKakaoToken();
+            if(token != null) {
+              KakaoUserInfo kakaoUser = await provider.getKakaoUserInfo(token);
+              handleSignIn(kakaoUser.email, kakaoUser.nickname, SignInPlatform.KAKAO);
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _googleBtn() {
+    return Card(
+      elevation: 18.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.hardEdge,
+      color: white,
+      child: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Ink.image(
+          image: const AssetImage('assets/images/google_logo.png'),
+          width: 34,
+          height: 34,
+          child: InkWell(
+            borderRadius: const BorderRadius.all(Radius.circular(35.0)),
+            onTap: () async {
+              final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+              if(googleUser != null){
+                handleSignIn(googleUser.email, googleUser.displayName ?? "", SignInPlatform.GOOGLE);
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _naverBtn() {
+    return Card(
+      elevation: 18.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.hardEdge,
+      child: Ink.image(
+        image: const AssetImage('assets/images/naver_logo.png'),
+        width: 60,
+        height: 60,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(35.0),
+          ),
+          onTap: () async {
+            final NaverLoginResult naverUser = await FlutterNaverLogin.logIn();
+            handleSignIn(naverUser.account.email, naverUser.account.nickname, SignInPlatform.NAVER);
+          },
+        ),
+      ),
+    );
+  }
+
+  handleSignIn(String email, String nickname, SignInPlatform platform) async {
+    SignInResponse? response = await Provider.of<SignInProvider>(_context!, listen: false).signInByOAuth(email, nickname, platform);
+    if (response.user == null) {
+      // 회원가입 진행
+      Provider.of<UserProvider>(_context!, listen: false).setOAuthInfo(email, platform);
+      Navigator.push(_context!,MaterialPageRoute(builder: (BuildContext context) => const SignUp()));
+
+    } else {
+      // 로그인 진행
+      Provider.of<UserProvider>(_context!, listen: false).setUser(response.user!, email, platform);
+      Navigator.of(_context!).pushNamed(CustomNavigationBar.routeName);
+    }
   }
 }
