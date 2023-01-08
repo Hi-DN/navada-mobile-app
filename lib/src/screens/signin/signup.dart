b58e7dc894b81c1d9db769ee4741a429e4a10512
hanslelee <hssarah13@gmail.com>
2023-01-08 22:21:47 +0900

    회원가입 단순 체크 로직

--- lib/src/screens/signin/signup.dart
+++ lib/src/screens/signin/signup.dart
@@ -5,6 +5,7 @@ import 'package:navada_mobile_app/src/widgets/custom_appbar.dart';
 import 'package:navada_mobile_app/src/widgets/custom_navigation_bar.dart';
 import 'package:navada_mobile_app/src/widgets/long_circled_btn.dart';
 import 'package:navada_mobile_app/src/widgets/screen_size.dart';
+import 'package:navada_mobile_app/src/widgets/space.dart';
 import 'package:navada_mobile_app/src/widgets/text_style.dart';
 import 'package:provider/provider.dart';
 
@@ -17,78 +18,156 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   ScreenSize size = ScreenSize();
-  late String userName, userNickname, userPhoneNum, userAddress;
+  String userName="", userNickname="", userPhoneNum="", userAddress="";
+
+  final FocusNode _nameFNode = FocusNode();
+  final FocusNode _nicknameFNode = FocusNode();
+  final FocusNode _phoneNumFNode = FocusNode();
+  final FocusNode _addressFNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameFNode.dispose();
+    _nicknameFNode.dispose();
+    _phoneNumFNode.dispose();
+    _addressFNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
       appBar: CustomAppBar(titleText: "회원가입"),
-      body: Column(children: [
-        TextFormField(
-          style: styleR.copyWith(fontSize: size.getSize(16)),
-          onChanged: (value) {
-            setState(() { userName = value; });
-          },
-          decoration: InputDecoration(
-            hintText: "이름",
-            isDense: true,
-            contentPadding: EdgeInsets.symmetric(
-              horizontal: 0, vertical: size.getSize(10.0),
-            ),
-            enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
-            focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
-          ),
-        ),
-        TextFormField(
-          style: styleR.copyWith(fontSize: size.getSize(16)),
-          onChanged: (value) {
-            setState(() { userNickname = value; });
-          },
-          decoration: InputDecoration(
-            hintText: "닉네임",
-            isDense: true,
-            contentPadding: EdgeInsets.symmetric(
-              horizontal: 0, vertical: size.getSize(10.0),
-            ),
-            enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
-            focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
-          ),
-        ),
-        TextFormField(
-          style: styleR.copyWith(fontSize: size.getSize(16)),
-          onChanged: (value) {
-            setState(() { userPhoneNum = value; });
-          },
-          decoration: InputDecoration(
-            hintText: "폰번호",
-            isDense: true,
-            contentPadding: EdgeInsets.symmetric(
-              horizontal: 0, vertical: size.getSize(10.0),
-            ),
-            enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
-            focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
-          ),
-        ),
-        TextFormField(
-          style: styleR.copyWith(fontSize: size.getSize(16)),
-          onChanged: (value) {
-            setState(() { userAddress = value; });
-          },
-          decoration: InputDecoration(
-            hintText: "주소",
-            isDense: true,
-            contentPadding: EdgeInsets.symmetric(
-              horizontal: 0, vertical: size.getSize(10.0),
-            ),
-            enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
-            focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
-          ),
-        ),
-        LongCircledBtn(text: "회원가입하기", onTap: () async {
-          bool result = await Provider.of<UserProvider>(context, listen: false).signup(userName, userNickname, userPhoneNum, userAddress);
-          if(result) Navigator.of(context).pushNamed(CustomNavigationBar.routeName);
-        },)
-      ]),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: size.getSize(26)),
+        child: Column(children: [
+          const Space(height: 20),
+          _nameField(),
+          const Space(height: 20),
+          _nicknameField(),
+          const Space(height: 20),
+          _phoneNumField(),
+          const Space(height: 20),
+          _addressField(),
+          const Expanded(child: SizedBox()),
+          LongCircledBtn(text: "회원가입하기", onTap: () => handleSignUp()),
+          const Space(height: 40)
+        ]),
+      ),
     );
+  }
+
+  Widget _nameField() {
+    return TextFormField(
+      focusNode: _nameFNode,
+      style: styleR.copyWith(fontSize: size.getSize(16)),
+      onChanged: (value) {
+        setState(() { userName = value; });
+      },
+      decoration: InputDecoration(
+        hintText: "이름",
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 0, vertical: size.getSize(10.0),
+        ),
+        enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
+        focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
+      ),
+    );
+  }
+
+  Widget _nicknameField() {
+    return TextFormField(
+      focusNode: _nicknameFNode,
+      style: styleR.copyWith(fontSize: size.getSize(16)),
+      onChanged: (value) {
+        setState(() { userNickname = value; });
+      },
+      decoration: InputDecoration(
+        hintText: "닉네임",
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 0, vertical: size.getSize(10.0),
+        ),
+        enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
+        focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
+      ),
+    );
+  }
+
+  Widget _phoneNumField() {
+    return TextFormField(
+      focusNode: _phoneNumFNode,
+      style: styleR.copyWith(fontSize: size.getSize(16)),
+      onChanged: (value) {
+        setState(() { userPhoneNum = value; });
+      },
+      decoration: InputDecoration(
+        hintText: "폰번호",
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 0, vertical: size.getSize(10.0),
+        ),
+        enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
+        focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
+      ),
+    );
+  }
+
+  Widget _addressField() {
+    return TextFormField(
+      focusNode: _addressFNode,
+      style: styleR.copyWith(fontSize: size.getSize(16)),
+      onChanged: (value) {
+        setState(() { userAddress = value; });
+      },
+      decoration: InputDecoration(
+        hintText: "주소",
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 0, vertical: size.getSize(10.0),
+        ),
+        enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: grey183)),
+        focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: green)),
+      ),
+    );
+  }
+
+  handleSignUp() async {
+    if (!_checkValid(userName)) {
+      _checkField(_nameFNode, "이름을 입력해주세요!");
+    } else if (!_checkValid(userNickname)) {
+      _checkField(_nicknameFNode, "닉네임을 입력해주세요!");
+    } else if (!_checkValid(userPhoneNum)) {
+      _checkField(_phoneNumFNode, "핸드폰 번호를 입력해주세요!");
+    } else if (!_checkValid(userAddress)) {
+      _checkField(_addressFNode, "주소를 입력해주세요!");
+    } else {
+      Provider.of<UserProvider>(context, listen: false).signup(userName, userNickname, userPhoneNum, userAddress)
+          .then((value) => Navigator.of(context).pushNamed(CustomNavigationBar.routeName));
+    }
+  }
+
+  bool _checkValid(String? value) {
+    if(value == null || value == "") return false;
+    return true;
+  }
+
+  _checkField(FocusNode fnode, String snackBarText) {
+    FocusScope.of(context).requestFocus(fnode);
+    _showSnackBarDurationForSec(snackBarText);
+  }
+
+  _showSnackBarDurationForSec(String snackBarText) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 1),
+      content: R16Text(text: snackBarText, textColor: white),
+    ));
   }
 }
